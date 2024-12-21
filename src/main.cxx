4e6d6f4a2aca260b91a37a98dbea4e9d9d7fca1c
Joseph McKenna <dev@jtkmckenna.com>
2024-12-21 14:32:35 +0000

    Finish Chapter 7

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,6 @@
 #include "rtweekend.hpp"
 
+#include "camera.hpp"
 #include "hittable.hpp"
 #include "hittable_list.hpp"
 #include "sphere.hpp"
@@ -20,17 +21,6 @@ double hit_sphere(const point3 &center, double radius, const ray &r) {
   }
 }
 
-colour ray_colour(const ray &r, const hittable &world) {
-  hit_record rec;
-  if (world.hit(r, interval(0, infinity), rec)) {
-    return 0.5 * (rec.normal + colour(1, 1, 1));
-  }
-
-  vec3 unit_direction = unit_vector(r.direction());
-  auto a = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - a) * colour(1.0, 1.0, 1.0) + a * colour(0.5, 0.7, 1.0);
-}
-
 int main() {
 
   // Image
@@ -48,45 +38,10 @@ int main() {
   world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
   world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-  // Camera
+  camera cam;
 
-  auto focal_length = 1.0;
-  auto viewport_height = 2.0;
-  auto viewport_width = viewport_height * (double(image_width) / image_height);
-  auto camera_center = point3(0, 0, 0);
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
 
-  // Calculate the vectors across the horizontal and down the vertical viewport
-  // edges.
-  auto viewport_u = vec3(viewport_width, 0, 0);
-  auto viewport_v = vec3(0, -viewport_height, 0);
-
-  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-  auto pixel_delta_u = viewport_u / image_width;
-  auto pixel_delta_v = viewport_v / image_height;
-
-  // Calculate the location of the upper left pixel.
-  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
-                             viewport_u / 2 - viewport_v / 2;
-  auto pixel00_loc =
-      viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-  // Render
-
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-  for (int j = 0; j < image_height; j++) {
-    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
-              << std::flush;
-    for (int i = 0; i < image_width; i++) {
-      auto pixel_center =
-          pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-      auto ray_direction = pixel_center - camera_center;
-      ray r(camera_center, ray_direction);
-
-      colour pixel_colour = ray_colour(r, world);
-      write_colour(std::cout, pixel_colour);
-    }
-  }
-
-  std::clog << "\rDone.                 \n";
+  cam.render(world);
 }
