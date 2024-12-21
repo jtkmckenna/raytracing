50afa687150fedbf78f5e086adeb289b87b0e74f
Joseph McKenna <dev@jtkmckenna.com>
2024-12-21 14:39:59 +0000

    Chapter 8: Antialiasing

--- include/colour.hpp
+++ include/colour.hpp
@@ -1,6 +1,7 @@
 #ifndef colour_H
 #define colour_H
 
+#include "interval.hpp"
 #include "vec3.hpp"
 
 #include <iostream>
@@ -13,9 +14,10 @@ void write_colour(std::ostream &out, const colour &pixel_colour) {
   auto b = pixel_colour.z();
 
   // Translate the [0,1] component values to the byte range [0,255].
-  int rbyte = int(255.999 * r);
-  int gbyte = int(255.999 * g);
-  int bbyte = int(255.999 * b);
+  static const interval intensity(0.000, 0.999);
+  int rbyte = int(256 * intensity.clamp(r));
+  int gbyte = int(256 * intensity.clamp(g));
+  int bbyte = int(256 * intensity.clamp(b));
 
   // Write out the pixel colour components.
   out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
