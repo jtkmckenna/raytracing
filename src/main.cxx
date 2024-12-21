0a7022351057a62445f9ca7b14e33d6c938fdf59
Joseph McKenna <dev@jtkmckenna.com>
2024-12-21 13:52:29 +0000

    Apply clang-format

--- src/main.cxx
+++ src/main.cxx
@@ -5,23 +5,23 @@
 
 int main() {
 
-    // Image
+  // Image
+  int image_width = 256;
+  int image_height = 256;
 
-    int image_width = 256;
-    int image_height = 256;
+  // Render
 
-    // Render
+  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_color = colour(double(i)/(image_width-1), double(j)/(image_height-1), 0);
-            write_colour(std::cout, pixel_color);
-        }
+  for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
+              << std::flush;
+    for (int i = 0; i < image_width; i++) {
+      auto pixel_color = colour(double(i) / (image_width - 1),
+                                double(j) / (image_height - 1), 0);
+      write_colour(std::cout, pixel_color);
     }
+  }
 
-    std::clog << "\rDone.                 \n";
+  std::clog << "\rDone.                 \n";
 }
-
