fc46cda40b4cd20911a4e476779ab023ad7b9c17
Joseph McKenna <dev@jtkmckenna.com>
2024-12-24 13:31:42 +0000

    Split shere.hpp

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -2,42 +2,16 @@
 #define SPHERE_H
 
 #include "hittable.hpp"
+#include "interval.hpp"
 #include "rtweekend.hpp"
 #include "vec3.hpp"
 
 class sphere : public hittable {
 public:
   sphere(const point3 &center, const double radius,
-         std::shared_ptr<material> mat)
-      : center(center), radius(std::fmax(0, radius)), mat(mat) {}
+         std::shared_ptr<material> mat);
 
-  bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
-    vec3 oc = center - r.origin();
-    CONST_VAR auto a = r.direction().length_squared();
-    CONST_VAR auto h = dot(r.direction(), oc);
-    CONST_VAR auto c = oc.length_squared() - radius * radius;
-
-    CONST_VAR auto discriminant = h * h - a * c;
-    if (discriminant < 0)
-      return false;
-    ASSUME(discriminant >= 0);
-    CONST_VAR auto sqrtd = std::sqrt(discriminant);
-
-    // Find the nearest root that lies in the acceptable range.
-    auto root = (h - sqrtd) / a;
-    if (!ray_t.surrounds(root)) {
-      root = (h + sqrtd) / a;
-      if (!ray_t.surrounds(root))
-        return false;
-    }
-
-    rec.t = root;
-    rec.p = r.at(rec.t);
-    CONST_VAR vec3 outward_normal = (rec.p - center) / radius;
-    rec.set_face_normal(r, outward_normal);
-    rec.mat = mat;
-    return true;
-  }
+  bool hit(const ray &r, interval ray_t, hit_record &rec) const override;
 
 private:
   point3 center;
