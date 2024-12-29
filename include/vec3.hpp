a39af09b4d3f7365376405f5a55ecf016a37b460
Joseph McKenna <dev@jtkmckenna.com>
2024-12-29 13:41:06 +0000

    Make more functions constexpr'able

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -13,12 +13,12 @@ public:
   vec3();
   CONSTEXPR vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-  double x() const;
-  double y() const;
-  double z() const;
+  CONSTEXPR double x() const { return e[0]; }
+  CONSTEXPR double y() const { return e[1]; }
+  CONSTEXPR double z() const { return e[2]; }
 
-  vec3 operator-() const;
-  double operator[](const int i) const;
+  CONSTEXPR vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+  CONSTEXPR double operator[](const int i) const { return e[i]; }
   double &operator[](const int i);
 
   vec3 &operator+=(const vec3 &v);
@@ -46,7 +46,7 @@ using point3 = vec3;
 
 // Vector Utility Functions
 
-CONSTEXPR_OR_INLINE std::ostream &operator<<(std::ostream &out, const vec3 &v) {
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
   return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
